7f949c06895cd3bb8fda2bbf5490de35b64a5fec
massimoalbarello <massimo00albarello@gmail.com>
2023-06-20 17:15:14 +0200

    generate signed uniqu access key

--- src/omnia-client-sdk/src/access_key.rs
+++ src/omnia-client-sdk/src/access_key.rs
@@ -1,5 +1,8 @@
-use omnia_core_sdk::access_key::UniqueAccessKey;
+use omnia_core_sdk::{access_key::UniqueAccessKey, signature::SignatureReply};
 
-pub fn sign_unique_access_key(unique_access_key: UniqueAccessKey) {
-    todo!("sign_unique_access_key {:?}", unique_access_key)
+pub async fn gnerate_signed_unique_access_key(unique_access_key: UniqueAccessKey) -> Result<SignatureReply, String> {
+    Ok(SignatureReply {
+        signature_hex: hex::encode(unique_access_key.generate_signature().await?.signature),
+        unique_access_key,
+    })
 }
