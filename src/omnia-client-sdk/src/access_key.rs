1ea9c3818a4b894629c63fb6b7366d017bd51cf9
massimoalbarello <massimo00albarello@gmail.com>
2023-06-20 17:20:53 +0200

    typo

--- src/omnia-client-sdk/src/access_key.rs
+++ src/omnia-client-sdk/src/access_key.rs
@@ -1,6 +1,6 @@
 use omnia_core_sdk::{access_key::UniqueAccessKey, signature::SignatureReply};
 
-pub async fn gnerate_signed_unique_access_key(unique_access_key: UniqueAccessKey) -> Result<SignatureReply, String> {
+pub async fn generate_signed_unique_access_key(unique_access_key: UniqueAccessKey) -> Result<SignatureReply, String> {
     Ok(SignatureReply {
         signature_hex: hex::encode(unique_access_key.generate_signature().await?.signature),
         unique_access_key,
