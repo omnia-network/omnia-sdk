e083a9a9cecbbfc78aa8dfde6b3423fa68bb9550
Luca8991 <luca.berte98@hotmail.it>
2023-06-22 17:19:35 +0200

    fix: add idenpotent-key header

--- src/omnia-core-sdk/src/http.rs
+++ src/omnia-core-sdk/src/http.rs
@@ -1,8 +1,13 @@
 use ic_cdk::api::management_canister::http_request::HttpHeader;
+use rand::Rng;
 
-use crate::access_key::{generate_signed_unique_access_key, AccessKeyUID};
+use crate::{
+    access_key::{generate_signed_unique_access_key, AccessKeyUID},
+    INIT_PARAMS_REF_CELL,
+};
 
 /// Returns the headers to be used in the HTTP requests to the device URL.
+/// It includes the access key headers and the idempotency header, along with some additional headers.
 ///
 /// * `access_key` - the access key obtained with the [request_access_key](crate::access_key::request_access_key) method.
 /// * `device_headers` - (optional) the headers returned by the query to the RDF database to get the device URL.
@@ -12,26 +17,35 @@ pub async fn get_request_headers(
 ) -> Result<Vec<HttpHeader>, String> {
     let singed_access_key = generate_signed_unique_access_key(access_key).await?;
 
+    let idempotent_key: u64 = INIT_PARAMS_REF_CELL
+        .with(|params| params.borrow_mut().rng().clone())
+        .gen();
+
     let mut headers = device_headers.unwrap_or(vec![]);
 
     headers.extend(vec![
         // access key headers
         HttpHeader {
-            name: "X-Omnia-Access-Key".to_string(),
+            name: String::from("X-Omnia-Access-Key"),
             value: singed_access_key.unique_access_key.get_key(),
         },
         HttpHeader {
-            name: "X-Omnia-Access-Key-Nonce".to_string(),
+            name: String::from("X-Omnia-Access-Key-Nonce"),
             value: singed_access_key.unique_access_key.get_nonce().to_string(),
         },
         HttpHeader {
-            name: "X-Omnia-Access-Key-Signature".to_string(),
+            name: String::from("X-Omnia-Access-Key-Signature"),
             value: singed_access_key.signature_hex,
+        },
+        // idempotency header
+        HttpHeader {
+            name: String::from("Idempotent-Key"),
+            value: idempotent_key.to_string(),
         },
         // additional headers
         HttpHeader {
-            name: "Content-Type".to_string(),
-            value: "application/json".to_string(),
+            name: String::from("Content-Type"),
+            value: String::from("application/json"),
         },
     ]);
 
